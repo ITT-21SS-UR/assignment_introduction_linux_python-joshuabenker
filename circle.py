f7939bfd05b171f31c7e3a8eb0645866462be89a
joshuabenker <joshuabenker@joshua.joshua>
2021-04-19 22:43:47 +0200

    count.sh, stats.py, circle.py done

--- circle.py
+++ circle.py
@@ -1,1 +1,34 @@
-print("TEST")
+#!/usr/bin/python3
+
+"""
+**circle.py**: Takes a *radius* (int) as commandline parameter. 
+Draws something resembling a circle with the given radius using the `turtle` module. 
+The turtle should alternatively move forward or turn left/right so that 
+it stays at about *radius* pixels from the center of the screen. 
+Experiment with varying turning angles and movement distances. 
+Do not use the `circle()` function - that would be too easy.
+"""
+import turtle
+import sys
+import math
+
+def drawCircle():
+    #set radius from input
+    radius = float(sys.argv[1])
+    turtle.penup()
+    #set start position
+    turtle.setposition(radius, 0.00)
+    #set pen values
+    turtle.pen(fillcolor="yellow", pencolor="green", pensize=10)
+    #pen "on"
+    turtle.pendown()
+    turtle.left(90)
+    #draw circle with one degree per step
+    for i in range(0,360):
+        turtle.forward(2 * math.pi * radius / 360)
+        turtle.left(1)
+
+if __name__ == "__main__":
+    drawCircle()
+
+    
--- count.sh
+++ count.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
 
+#check if the file is present in the current directory
+if ! [[ -f 'README' ]]; then 
+  #if it's not yet present -> download the file with "wget"
+  wget ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README  
+fi
+#read file
+cat README |
+#make all text lowercase
+tr '[:upper:]' '[:lower:]' |#!/bin/bash
+
+#check if the file is present in the current directory
+if ! [[ -f 'README' ]]; then 
+  #if it's not yet present -> download the file with "wget"
+  wget ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README  
+fi
+#read file
+cat README |
+#make all text lowercase
+tr '[:upper:]' '[:lower:]' |
+#replace whitespace with new line
+sed "s/ /\n/g" |
+#remove all empty lines
+sed '/^$/d' |
+#remove punctation
+tr -d [:punct:] |
+#alphabetically sort the list of words and remove duplicates
+sort | uniq -c |
+#print out the 10 most common words in the text on stdout
+sort -r | awk '{print $2}' | head -n 10
+#replace whitespace with new line
+sed "s/ /\n/g" |
+#remove all empty lines
+sed '/^$/d' |
+#remove punctation
+tr -d [:punct:] |
+#alphabetically sort the list of words and remove duplicates
+sort | uniq -c |
+#print out the 10 most common words in the text on stdout
+sort -r | awk '{print $2}' | head -n 10
