904dd229d4aadbf935f0ca73842067301141d2bd
root <root@joshua.joshua>
2021-04-19 23:32:36 +0200

    add clone

--- circle.py
+++ circle.py
@@ -13,21 +13,42 @@ import math
 
 
 def drawCircle():
-
     # set radius from input
     radius = float(sys.argv[1])
-    turtle.penup()
+    # set background color
+    turtle.bgcolor("blue")
+    # new turtle "fred"
+    fred = turtle.Turtle()
+    # pen "off"
+    fred.penup()
     # set start position
-    turtle.setposition(radius, 0.00)
+    fred.setposition(radius, 0.00)
     # set pen values
-    turtle.pen(fillcolor="yellow", pencolor="green", pensize=10)
+    fred.pen(pensize=10)
     # pen "on"
-    turtle.pendown()
-    turtle.left(90)
-    # draw circle with one degree per step
-    for i in range(0, 360):
-        turtle.forward(2 * math.pi * radius / 360)
-        turtle.left(1)
+    fred.pendown()
+    # set freds colors
+    fred.color("yellow", "green")
+    # new turtle "amy" joings
+    amy = fred.clone()
+    # set amys color
+    amy.color("yellow", "pink")
+    # start filling
+    fred.begin_fill()
+    amy.begin_fill()
+    fred.left(90)
+    amy.right(90)
+    # draw circle with one degree per stepo
+    # amy and fred draw together, each turtle a half one
+    for i in range(0, 180):
+        fred.forward(2 * math.pi * radius / 360)
+        fred.left(1)
+        amy.forward(2 * math.pi * radius / 360)
+        amy.right(1)
+    fred.end_fill()
+    amy.end_fill()
+    # exit screen via click
+    turtle.Screen().exitonclick()
 
 
 if __name__ == "__main__":
